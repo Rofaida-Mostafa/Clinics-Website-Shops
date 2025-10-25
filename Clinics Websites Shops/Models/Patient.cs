d5777d9df629f184473a082f1bb829e44ae7da29
Rofaida-Mostafa <rofaidaashraf333@gmail.com>
2025-10-25 22:37:27 +0300

    add

--- Clinics Websites Shops/Models/Patient.cs
+++ Clinics Websites Shops/Models/Patient.cs
@@ -3,7 +3,6 @@
     public class Patient
     {
         public string PatientId { get; set; }
-        public string? BloodType { get; set; }
         public ICollection<Appointment> Appointments { get; set; } = new List<Appointment>();
         public ICollection<Report> Reports { get; set; } = new List<Report>();
         public string ApplicationUserId { get; set; }
