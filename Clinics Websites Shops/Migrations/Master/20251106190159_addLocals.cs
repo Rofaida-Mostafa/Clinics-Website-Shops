194e4a682a994cc29680f41c73ca2f4930bc0b73
Rofaida-Mostafa <rofaidaashraf333@gmail.com>
2025-11-06 21:07:49 +0200

    Add Locals

---
+++ Clinics Websites Shops/Migrations/Master/20251106190159_addLocals.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Clinics_Websites_Shops.Migrations.Master
+{
+    /// <inheritdoc />
+    public partial class addLocals : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Locals",
+                table: "Tenants",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Locals",
+                table: "Tenants");
+        }
+    }
+}
